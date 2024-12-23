2acbe380ba02f14023ed2ea14dd5a0e2ace55104
Gokul Raj <serajgokul19@gmail.com>
2024-12-23 21:57:22 +0530

    models

--- e_commerce_app/models.py
+++ e_commerce_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('user', 'User'),
+    )
+
+    role = models.CharField(max_length=10, choice=ROLE_CHOICES, default='user')
+
+    def __str__(self):
+        return self.username
+    
+
+class Product(models):
+    name = models.CharField(max_length=50)
+    price = models.FloatField
--- e_commerce_app/views.py
+++ e_commerce_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
 def home(request):
     return render(request, "e_commerce_app/home.html")
+
+def signup(request):
+    return render(request, "e_commerce_app/signup.html")
+
+
+def login(request):
+    return render(request, "e_commerce_app/login.html")
